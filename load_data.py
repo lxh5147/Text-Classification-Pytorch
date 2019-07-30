dc1dfc06463c9b7c6c9f8775932066ec05171e8a
lxh5147 <lxh5147@126.com>
2019-07-30 12:55:52 +0800

    reformatted

--- load_data.py
+++ load_data.py
@@ -1,16 +1,12 @@
 # _*_ coding: utf-8 _*_
 
-import os
-import sys
 import torch
-from torch.nn import functional as F
-import numpy as np
 from torchtext import data
 from torchtext import datasets
-from torchtext.vocab import Vectors, GloVe
+from torchtext.vocab import GloVe
+
 
 def load_dataset(test_sen=None):
-
     """
     tokenizer : Breaks sentences into a list of words. If sequential=False, no tokenization is applied
     Field : A class that stores information about the way of preprocessing
@@ -25,21 +21,24 @@ def load_dataset(test_sen=None):
     BucketIterator : Defines an iterator that batches examples of similar lengths together to minimize the amount of padding needed.
     
     """
-    
+
     tokenize = lambda x: x.split()
-    TEXT = data.Field(sequential=True, tokenize=tokenize, lower=True, include_lengths=True, batch_first=True, fix_length=200)
+    TEXT = data.Field(sequential=True, tokenize=tokenize, lower=True, include_lengths=True, batch_first=True,
+                      fix_length=200)
     LABEL = data.LabelField(dtype=torch.FloatTensor)
     train_data, test_data = datasets.IMDB.splits(TEXT, LABEL)
     TEXT.build_vocab(train_data, vectors=GloVe(name='6B', dim=300))
     LABEL.build_vocab(train_data)
 
     word_embeddings = TEXT.vocab.vectors
-    print ("Length of Text Vocabulary: " + str(len(TEXT.vocab)))
-    print ("Vector size of Text Vocabulary: ", TEXT.vocab.vectors.size())
-    print ("Label Length: " + str(len(LABEL.vocab)))
+    print("Length of Text Vocabulary: " + str(len(TEXT.vocab)))
+    print("Vector size of Text Vocabulary: ", TEXT.vocab.vectors.size())
+    print("Label Length: " + str(len(LABEL.vocab)))
 
-    train_data, valid_data = train_data.split() # Further splitting of training_data to create new training_data & validation_data
-    train_iter, valid_iter, test_iter = data.BucketIterator.splits((train_data, valid_data, test_data), batch_size=32, sort_key=lambda x: len(x.text), repeat=False, shuffle=True)
+    train_data, valid_data = train_data.split()  # Further splitting of training_data to create new training_data & validation_data
+    train_iter, valid_iter, test_iter = data.BucketIterator.splits((train_data, valid_data, test_data), batch_size=32,
+                                                                   sort_key=lambda x: len(x.text), repeat=False,
+                                                                   shuffle=True)
 
     '''Alternatively we can also use the default configurations'''
     # train_iter, test_iter = datasets.IMDB.iters(batch_size=32)
--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-import os
-import time
-import load_data
+import numpy as np
 import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
-import torch.optim as optim
-import numpy as np
+
+import load_data
 from models.LSTM import LSTMClassifier
 
 TEXT, vocab_size, word_embeddings, train_iter, valid_iter, test_iter = load_data.load_dataset()
+
 
 def clip_gradient(model, clip_value):
     params = list(filter(lambda p: p.grad is not None, model.parameters()))
     for p in params:
         p.grad.data.clamp_(-clip_value, clip_value)
-    
+
+
 def train_model(model, train_iter, epoch):
     total_epoch_loss = 0
     total_epoch_acc = 0
@@ -29,25 +29,27 @@ def train_model(model, train_iter, epoch):
         if torch.cuda.is_available():
             text = text.cuda()
             target = target.cuda()
-        if (text.size()[0] is not 32):# One of the batch returned by BucketIterator has length different than 32.
+        if (text.size()[0] is not 32):  # One of the batch returned by BucketIterator has length different than 32.
             continue
         optim.zero_grad()
         prediction = model(text)
         loss = loss_fn(prediction, target)
         num_corrects = (torch.max(prediction, 1)[1].view(target.size()).data == target.data).float().sum()
-        acc = 100.0 * num_corrects/len(batch)
+        acc = 100.0 * num_corrects / len(batch)
         loss.backward()
         clip_gradient(model, 1e-1)
         optim.step()
         steps += 1
-        
+
         if steps % 100 == 0:
-            print (f'Epoch: {epoch+1}, Idx: {idx+1}, Training Loss: {loss.item():.4f}, Training Accuracy: {acc.item(): .2f}%')
-        
+            print(
+                f'Epoch: {epoch + 1}, Idx: {idx + 1}, Training Loss: {loss.item():.4f}, Training Accuracy: {acc.item(): .2f}%')
+
         total_epoch_loss += loss.item()
         total_epoch_acc += acc.item()
-        
-    return total_epoch_loss/len(train_iter), total_epoch_acc/len(train_iter)
+
+    return total_epoch_loss / len(train_iter), total_epoch_acc / len(train_iter)
+
 
 def eval_model(model, val_iter):
     total_epoch_loss = 0
@@ -66,12 +68,12 @@ def eval_model(model, val_iter):
             prediction = model(text)
             loss = loss_fn(prediction, target)
             num_corrects = (torch.max(prediction, 1)[1].view(target.size()).data == target.data).sum()
-            acc = 100.0 * num_corrects/len(batch)
+            acc = 100.0 * num_corrects / len(batch)
             total_epoch_loss += loss.item()
             total_epoch_acc += acc.item()
 
-    return total_epoch_loss/len(val_iter), total_epoch_acc/len(val_iter)
-	
+    return total_epoch_loss / len(val_iter), total_epoch_acc / len(val_iter)
+
 
 learning_rate = 2e-5
 batch_size = 32
@@ -85,9 +87,10 @@ loss_fn = F.cross_entropy
 for epoch in range(10):
     train_loss, train_acc = train_model(model, train_iter, epoch)
     val_loss, val_acc = eval_model(model, valid_iter)
-    
-    print(f'Epoch: {epoch+1:02}, Train Loss: {train_loss:.3f}, Train Acc: {train_acc:.2f}%, Val. Loss: {val_loss:3f}, Val. Acc: {val_acc:.2f}%')
-    
+
+    print(
+        f'Epoch: {epoch + 1:02}, Train Loss: {train_loss:.3f}, Train Acc: {train_acc:.2f}%, Val. Loss: {val_loss:3f}, Val. Acc: {val_acc:.2f}%')
+
 test_loss, test_acc = eval_model(model, test_iter)
 print(f'Test Loss: {test_loss:.3f}, Test Acc: {test_acc:.2f}%')
 
@@ -109,6 +112,6 @@ model.eval()
 output = model(test_tensor, 1)
 out = F.softmax(output, 1)
 if (torch.argmax(out[0]) == 1):
-    print ("Sentiment: Positive")
+    print("Sentiment: Positive")
 else:
-    print ("Sentiment: Negative")
+    print("Sentiment: Negative")
